68de36ba542f295292f9a321a82f7a551e29661b
runoob <test@runoob.com>
2025-12-01 15:40:09 +0800

    update

--- c++/1-5.cpp
+++ c++/1-5.cpp
@@ -1,13 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-int dp[5][2][2][2][2][2][2][2020],num[5],p[5];
-int dfs(int len,bool limit1,bool limit2,bool  limit3,bool ok1,bool ok2,bool ok3,bool sum){
-    if(!len)return sum=2019&&ok1&&ok2&&ok3;
-    if(-dp[len][limit1][limit2][limit3][ok1][ok2][ok3][sum])return dp[len][limit1][limit2][limit3][ok1][ok2][ok3][sum];
-    int res=0;
-    int up1=limit1?num[1]:9;
-    int up2=limit2?num[2]:9;
-    int up3=limit3?num[3]:9;
-    for(int i=0;i<=up1;i++){
-        if(i==2||i==4)
+
+bool check(int x){
+    while(x){
+        if(x%10 == 2 || x%10 == 4) return false;
+        x /= 10;
+    }
+    return true;
+}
+bool flag[2020];
+signed main(){
+    int ans = 0;
+    for (int i = 1; i <= 2019;i++)if(check(i)) flag[i] = 1;
+    for(int i=1;i<2019;i++)
+        for (int j=i+1; j<=2019; j++){
+            int k = 2019 - i - j;
+            if(j<k && k<= 2019 && flag[i] && flag[j] && flag[k])
+                ans++;
+        }
+    cout << ans << '\n';
+    return 0;
 }
