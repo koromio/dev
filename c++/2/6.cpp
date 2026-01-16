644a8e90370fe53dcf6b5a4864c8e664bf83db6d
runoob <test@runoob.com>
2026-01-16 10:17:33 +0800

    update

---
+++ c++/2/6.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Enter the number of light years:";
+    double lightYears;
+    cin>>lightYears;
+    cout<<lightYears<<" light years ="<<63240*lightYears<<" astronomical units";
+}
---
+++ c++/2/7.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter the number of hours:";
+    int hours;
+    cin >> hours;
+    cout << "Enter the number of minutes:";
+    int minutes;
+    cin >> minutes;
+    cout << "Time:" <<hours << ":" << minutes << endl;
+}
